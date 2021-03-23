1ed6062c6a37c3143c7fba3ad775b02082b4e2bc
Nick Huppert <hupsmann@gmail.com>
2021-03-23 16:39:49 +1100

    Add updated rust file

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -87,6 +87,43 @@ decl_module! {
 		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
 
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			Something::put(something);
+
+			// Emit an event.
+			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			// Return a successful DispatchResult
+			Ok(())
+		}
+
+		/// An example dispatchable that may throw a custom error.
+		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
+		pub fn cause_error(origin) -> dispatch::DispatchResult {
+			let _who = ensure_signed(origin)?;
+
+			// Read a value from storage.
+			match Something::get() {
+				// Return an error if the value has not been set.
+				None => Err(Error::<T>::NoneValue)?,
+				Some(old) => {
+					// Increment the value read from storage; will error in the event of overflow.
+					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					// Update the value in storage with the incremented result.
+					Something::put(new);
+					Ok(())
+				},
+			}
+		}
+
 		/// DAPP Addition blocks must be block x2
 		/// Allow a user to claim ownership of an unclaimed proof.
         #[weight = 10_000]
@@ -132,42 +169,5 @@ decl_module! {
             // Emit an event that the claim was erased.
             Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
         }
-
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			Something::put(something);
-
-			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			// Return a successful DispatchResult
-			Ok(())
-		}
-
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
-
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
-		}
 	}
 }
